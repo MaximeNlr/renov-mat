4d972b5bfe2a404144f2df7bf199905614cd5d67
MaximeNlr <158460952+MaximeNlr@users.noreply.github.com>
2025-08-21 15:36:06 +0200

    test cookie

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -52,7 +52,7 @@ exports.signup = async (req, res, next) => {
       res.cookie('token', token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+        sameSite: 'None',
         maxAge: 24 * 60 * 60 * 1000
       });
   
