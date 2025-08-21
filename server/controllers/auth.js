612675221a9676b480aa0b1d2a54eae0e3438e2b
MaximeNlr <158460952+MaximeNlr@users.noreply.github.com>
2025-08-21 17:12:34 +0200

    fix cookie

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -52,7 +52,7 @@ exports.signup = async (req, res, next) => {
       res.cookie('token', token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        sameSite: 'lax',
+        sameSite: 'None',
         path: '/',
         maxAge: 24 * 60 * 60 * 1000
       });
